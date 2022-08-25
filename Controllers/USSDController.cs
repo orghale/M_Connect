092b4bc8324428f802303d98b72f1ca4d51ee62e
Oghale Ekaba <oghaleykg@gmail.com>
2022-08-26 00:50:38 +0100

    20220826

--- Controllers/USSDController.cs
+++ Controllers/USSDController.cs
@@ -9,7 +9,7 @@ namespace M_Connect.Controllers
 {
     [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    // [Route("ussd")]
     public class USSDController : ControllerBase
     {
 
@@ -21,13 +21,14 @@ namespace M_Connect.Controllers
         }
 
 
-        [HttpGet]
-        [ProducesResponseType(typeof(Response_Object), 200)]
-        public async Task<IActionResult> Get(string code)
+        [HttpPost("callback")]
+        [ProducesResponseType(typeof(CallbackRes), 200)]
+        public async Task<IActionResult> CallbackAction(Callback r)
         {
             try
             {
-                if (string.IsNullOrWhiteSpace(code))
+
+                if (!ModelState.IsValid)
                 {
                     _logger.LogInformation($"Invalid request: Code not valid");
                     return StatusCode(400, $"Invalid request: Code not valid");
@@ -35,25 +36,155 @@ namespace M_Connect.Controllers
 
                 var links = new List<Link>();
 
+                var response = new CallbackRes
+                {
+                    url = $"{ new UriBuilder(Request.Scheme, Request.Host.Host, Request.Host.Port.Value, "ussd")}",
+                    timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff"),
+                    state = new State
+                    {
+                        description = "Outgoing session - Message sent",
+                        code = 1
+                    },
+                    split_page = 0,
+                    alias = r.alias
+                };
+
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex}");
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpGet("ussd")]
+        [ProducesResponseType(typeof(Response_Object), 200)]
+        public async Task<IActionResult> USSD(string code)
+        {
+            try
+            {
+
+                if (string.IsNullOrWhiteSpace(code))
+                {
+                    _logger.LogInformation($"Invalid request: Code not valid");
+                    return StatusCode(400, $"Invalid request: Code not valid");
+                }
+
+               // var links = new List<Link>();
+
                 var response = new Response_Object
                 {
                     title = "Your Service:",
                     message = "Ask your question",
                     form = new Form
                     {
-                        url = "https://your_service_host:port/question",
+                        url = $"{ new UriBuilder(Request.Scheme, Request.Host.Host, Request.Host.Port.Value, "question")}",
                         type = "text",
-                        method = "get"
+                        method = "get",
+                        input = "user-response-to-question"
                     },
                     links = new List<Link>() {
-                        new Link {content = "My account",url= "https://your_service_host:port/account" },
-                        new Link {content ="Terms and conditions",url= "https://your_service_host:port/TandC" }
+                        new Link {content = "My account",url= $"{ new UriBuilder(Request.Scheme, Request.Host.Host, Request.Host.Port.Value, "account")}" },
+                        new Link {content ="Terms and conditions",url= $"{ new UriBuilder(Request.Scheme, Request.Host.Host, Request.Host.Port.Value, "TandC")}" }
                     },
                     page = new Page
                     {
                         menu = "true",
                         history = "true",
-                        navigation_keywords = "true"
+                        navigation_keywords = "true",
+                        callback = $"{ new UriBuilder(Request.Scheme, Request.Host.Host, Request.Host.Port.Value, "callback")}",
+                        method = "Post"
+                    }
+                };
+
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex}");
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+
+        [HttpGet("account")]
+        [ProducesResponseType(typeof(Response_Object), 200)]
+        public async Task<IActionResult> MyAccount()
+        {
+            try
+            {
+              
+                var response = new Response_Object
+                {
+                    title = "My account:",
+                    message = "Press 1 to continue",
+                    //form = new Form
+                    //{
+                    //    url = "https://your_service_host:port/question",
+                    //    type = "text",
+                    //    method = "get"
+                    //},
+                    //links = new List<Link>() {
+                    //    new Link {content = "My account",url= "https://your_service_host:port/account" },
+                    //    new Link {content ="Terms and conditions",url= "https://your_service_host:port/TandC" }
+                    //},
+                    page = new Page
+                    {
+                        menu = "true",
+                        history = "true",
+                        navigation_keywords = "true",
+                        callback = $"{ new UriBuilder(Request.Scheme, Request.Host.Host, Request.Host.Port.Value, "callback")}",
+                        method = "Post"
+                    }
+                };
+
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex}");
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+
+        [HttpGet("TandC")]
+        [ProducesResponseType(typeof(Response_Object), 200)]
+        public async Task<IActionResult> TermsandConditions()
+        {
+            try
+            {
+               
+                var links = new List<Link>();
+
+                var response = new Response_Object
+                {
+                    title = "Terms and conditions:",
+                    message = "Press 1 to continue",
+                    //form = new Form
+                    //{
+                    //    url = "https://your_service_host:port/question",
+                    //    type = "text",
+                    //    method = "get"
+                    //},
+                    //links = new List<Link>() {
+                    //    new Link {content = "My account",url= "https://your_service_host:port/account" },
+                    //    new Link {content ="Terms and conditions",url= "https://your_service_host:port/TandC" }
+                    //},
+                    page = new Page
+                    {
+                        menu = "true",
+                        history = "true",
+                        navigation_keywords = "true",
+                        callback = $"{ new UriBuilder(Request.Scheme, Request.Host.Host, Request.Host.Port.Value, "callback")}",
+                        method = "Post"
                     }
                 };
 
--- ModelObject.cs
+++ ModelObject.cs
@@ -8,6 +8,7 @@ namespace M_Connect
     {
         public string url { get; set; }
         public string type { get; set; }
+        public string input { get; set; }
         public string method { get; set; }
     }
 
@@ -22,8 +23,16 @@ namespace M_Connect
         public string menu { get; set; }
         public string history { get; set; }
         public string navigation_keywords { get; set; }
-    }
+        public string session_end { get; set; } = null;
+        public string callback { get; set; } = null;
+        public string method { get; set; } = null;
 
+    }
+    public class Push
+    {
+        public string url { get; set; }
+        public string callback { get; set; }
+    }
     public class Response_Object
     {
         public string title { get; set; }
@@ -32,6 +41,35 @@ namespace M_Connect
         public Form form { get; set; }
         public List<Link> links { get; set; }
         public Page page { get; set; }
+        public Push push { get; set; } = null;
     }
+
+
+    public class Callback
+    {
+        public string url { get; set; }
+        public DateTime timestamp { get; set; }
+        public State state { get; set; }
+        public int split_page { get; set; }
+        public string session_state { get; set; }
+        public string alias { get; set; }
+    }
+
+    public class State
+    {
+        public string description { get; set; }
+        public int code { get; set; }
+    }
+
+    public class CallbackRes
+    {
+        public string url { get; set; }
+        public string timestamp { get; set; }
+        public State state { get; set; }
+        public int split_page { get; set; }
+        public string alias { get; set; }
+    }
+
+  
 
 }
