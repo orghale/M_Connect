acfc82ab529bbb39c75c94f125842f64b43f7c60
Oghale Ekaba <oghaleykg@gmail.com>
2022-08-26 00:58:32 +0100

    20220826-2

--- AuthContext/AuthorizeRequest.cs
+++ AuthContext/AuthorizeRequest.cs
@@ -10,7 +10,7 @@ using Microsoft.Net.Http.Headers;
 namespace M_Connect.Controllers
 {
 
-    [AttributeUsage(validOn: AttributeTargets.Class)]
+    [AttributeUsage(validOn: AttributeTargets.Method)]
     public class Authorize : Attribute, IAsyncActionFilter
     {
         private const string sid = "sid";
@@ -18,38 +18,38 @@ namespace M_Connect.Controllers
 
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-           // var sidHeader = context.HttpContext.Request.Headers.TryGetValue(sid, out var extractedsid);
-           // var authHeader = context.HttpContext.Request.Headers.TryGetValue(auth, out var extractedauth);
+            var sidHeader = context.HttpContext.Request.Headers.TryGetValue(sid, out var extractedsid);
+            var authHeader = context.HttpContext.Request.Headers.TryGetValue(auth, out var extractedauth);
 
-            //if (!sidHeader && !authHeader)
-            //{
-            //    context.Result = new ContentResult()
-            //    {
-            //        StatusCode = 401,
-            //        Content = "Unauthorized Access: Request missing header authorization"
-            //    };
-            //    return;
-            //}
+            if (!sidHeader && !authHeader)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Unauthorized Access: Request missing header authorization"
+                };
+                return;
+            }
 
-            //if (!sidHeader)
-            //{
-            //    context.Result = new ContentResult()
-            //    {
-            //        StatusCode = 401,
-            //        Content = "Unauthorized Access: SID was not provided"
-            //    };
-            //    return;
-            //}
+            if (!sidHeader)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Unauthorized Access: SID was not provided"
+                };
+                return;
+            }
 
-            //if (!authHeader)
-            //{
-            //    context.Result = new ContentResult()
-            //    {
-            //        StatusCode = 401,
-            //        Content = "Unauthorized Access: AUTH was not provided"
-            //    };
-            //    return;
-            //}
+            if (!authHeader)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Unauthorized Access: AUTH was not provided"
+                };
+                return;
+            }
 
 
             var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
@@ -57,27 +57,27 @@ namespace M_Connect.Controllers
             var sidKey = appSettings.GetSection("ApiKeyConfig").GetValue<string>("sid");
             var authKey = appSettings.GetSection("ApiKeyConfig").GetValue<string>("auth");
 
-            ////validate request sid value against system sid key 
-            //if (!sidKey.Equals(extractedsid))
-            //{
-            //    context.Result = new ContentResult()
-            //    {
-            //        StatusCode = 401,
-            //        Content = "Unauthorized Access: SID is not valid"
-            //    };
-            //    return;
-            //}
+            //validate request sid value against system sid key 
+            if (!sidKey.Equals(extractedsid))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Unauthorized Access: SID is not valid"
+                };
+                return;
+            }
 
-            ////validate request auth value against system auth key 
-            //if (!authKey.Equals(extractedauth/*.ToString().DecryptString()*/))
-            //{
-            //    context.Result = new ContentResult()
-            //    {
-            //        StatusCode = 401,
-            //        Content = "Unauthorized Access: AUTH is not valid"
-            //    };
-            //    return;
-            //}
+            //validate request auth value against system auth key 
+            if (!authKey.Equals(extractedauth/*.ToString().DecryptString()*/))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Unauthorized Access: AUTH is not valid"
+                };
+                return;
+            }
 
             //add request sid value to response header 
             context.HttpContext.Response.Headers.Add(sid, sidKey.ToString());
